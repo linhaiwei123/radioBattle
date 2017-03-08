243ef1d9c45677d0633dc1042ab5992077a7e7a2
linhaiwei123 <15986711808@163.com>
2017-03-08 18:31:24 +0800

    cmd mgr done due with result mgr

--- assets/battle-module/battle-cmd-mgr.js
+++ assets/battle-module/battle-cmd-mgr.js
@@ -7,8 +7,10 @@ cc.Class({
         _target: cc.Node,
         _targetData: null,
         _touchFsm: null,
-        _currentTouchPosition: null,
+        _currentWorldTouchPosition: null,
+        _worldDestinationPosition: null,
         movePixel: 2,
+        _dragLineCtx: null,
     },
 
     onLoad: function(){
@@ -17,7 +19,7 @@ cc.Class({
         this.cmdTouchPanel.on("touchend",this.onTouchEnd.bind(this));
         this._touchFsm = require("battle-touch-fsm");
         this._touchFsm.startup();
-
+        this._dragLineCtx = this.cmdTouchPanel.getComponent(cc.Graphics);
     },
 
     cmd: function (event,from,to,playerData) {
@@ -38,16 +40,33 @@ cc.Class({
 
     controlAbleUpdate: function(){
         if(this._target && this._touchFsm){
-            if(this._touchFsm.current == 'moving' && this._currentTouchPosition){
+            if(this._touchFsm.current == 'moving' && this._currentWorldTouchPosition){
                 //follow the point
                 //delete the line gizmo
-                let moveNormalVector = cc.pNormalize(cc.pSub(this._currentTouchPosition,this._target.position));
+                //#
+                let moveNormalVector = cc.pNormalize(this._target.convertToNodeSpaceAR(this._currentWorldTouchPosition));
                 this._target.position = cc.pAdd(this._target.position,cc.pMult(moveNormalVector,this.movePixel));
             }
-            else if(this._touchFsm.current == 'dragging' && this._currentTouchPosition){
+            else if(this._touchFsm.current == 'dragging' && this._currentWorldTouchPosition){
                 //dragging line
+                //gizmo render
+                //let startPointInCtx = this.cmdTouchPanel.convertToNodeSpaceAR(this._target.parent.convertToWorldSpaceAR(this._target.position));
+                let startPointInCtx = this._target.parent.convertToWorldSpaceAR(this._target.position);
+                let endPointInCtx = this.getEndPointInCtx(this._currentWorldTouchPosition);
+                this._dragLineCtx.clear();
+                this._dragLineCtx.moveTo(startPointInCtx.x,startPointInCtx.y);
+                this._dragLineCtx.lineTo(endPointInCtx.x,endPointInCtx.y);
+                this._dragLineCtx.stroke();
             }
         }
+    },
+
+    getEndPointInCtx: function(worldTouchPosition){
+        let worldPositionOfTarget = this._target.parent.convertToWorldSpaceAR(this._target.position);
+        let worldDestinationPosition = cc.pAdd(worldPositionOfTarget,cc.pMult(cc.pSub(worldPositionOfTarget,worldTouchPosition),2));
+        //return this.cmdTouchPanel.convertToNodeSpaceAR(worldDestinationPosition);
+        this._worldDestinationPosition = worldDestinationPosition;
+        return worldDestinationPosition;
     },
 
     onTouchStart: function(e){
@@ -57,24 +76,25 @@ cc.Class({
             if(this._targetData.radio >= touchPostionInTarget.mag()){
                 this._touchFsm["touchstart-inner"]();
             }
-            this._currentTouchPosition = this.node.convertToNodeSpaceAR(e.getTouches()[0].getLocation());
+            this._currentWorldTouchPosition = e.getTouches()[0].getLocation();
         }
         if(this._touchFsm.current == 'dragging' && e.getTouches().length >=2){
-            this._currentTouchPosition = null;
+            this._currentWorldTouchPosition = null;
             this._touchFsm["touchstart-multi"]();
-        }else{
-            this._currentTouchPosition = this.node.convertToNodeSpaceAR(e.getTouches()[0].getLocation());
         }
+        // else{
+        //     this._currentWorldTouchPosition = e.getTouches()[0].getLocation();
+        // }
         
     },
 
     onTouchMove: function(e){
         //console.log("touchMove");
         if(this._touchFsm.current == 'dragging' && e.getTouches().length >=2){
-            this._currentTouchPosition = null;
+            this._currentWorldTouchPosition = null;
             this._touchFsm["touchmove-multi"]();
         }else{
-            this._currentTouchPosition = this.node.convertToNodeSpaceAR(e.getTouches()[0].getLocation());
+            this._currentWorldTouchPosition = e.getTouches()[0].getLocation();
         }
     },
 
@@ -83,8 +103,13 @@ cc.Class({
         if(this._touchFsm.current == 'dragging' && e.getTouches().length == 1){
             //release
             console.log("dragging release");
+            this.cmdTouchPanel.active = false;
+            this.unSchedule(this.controlAbleUpdate.bind(this));
+            this._dragLineCtx.clear();
+            this.getComponent('battle-main-mgr')._battleFsm["cmd-end"]({targetData: this._targetData, worldDestinationPosition : this._worldDestinationPosition});
+
         }else if(this._touchFsm.current == 'moving'){
-            this._currentTouchPosition = null;
+            this._currentWorldTouchPosition = null;
         }
     },
 
---
+++ assets/battle-module/battle-result-mgr.js
@@ -0,0 +1,18 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+
+    result: function (event,from,to,arg) {
+        let targetData = arg.targetData;
+        let worldDestinationPosition = arg.worldDestinationPosition;
+        //init signal and throw it 
+        //test the default signal
+        
+    },
+
+
+});
