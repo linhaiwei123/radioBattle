84cd5924ca9805c1149a9c5f64b671fe6b72ee0b
linhaiwei123 <15986711808@163.com>
2017-03-08 08:46:01 +0800

    fix bug in cmd step

--- assets/battle-module/battle-race-mgr.js
+++ assets/battle-module/battle-race-mgr.js
@@ -15,17 +15,23 @@ cc.Class({
     },
 
     raceTick: function(){
+        let touchActiveMaxArray = [];
         for(let playerData of this._playerDatas){
             //check alive
             if(playerData.blood <= 0){continue;}
             playerData.actionCurValue += playerData.speed;
             console.log(playerData.actionCurValue);
             if(playerData.actionCurValue >= playerData.actionMaxValue){
-                this.unschedule(this.raceTick);
-                this.getComponent("battle-main-mgr")._battleFsm["race-end"](playerData);
+                //this.unschedule(this.raceTick);
+                //this.scheduleOnce(this.getComponent("battle-main-mgr")._battleFsm["race-end"](playerData).bind(this),0);
+                touchActiveMaxArray.push(playerData);
             }
         }
-        
+        if(!!touchActiveMaxArray.length){
+            this.unschedule(this.raceTick);
+            touchActiveMaxArray.sort(function(a,b){ return a.actionCurValue - b.activeCurValue});
+            this.getComponent("battle-main-mgr")._battleFsm["race-end"](playerData);
+        }
     },
 
 });
